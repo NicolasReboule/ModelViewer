d204b5d84484cc7f74cc0f0b64f23bd565cae340
NicolasReboule <nicolas.reboule@epitech.eu>
2025-12-21 18:06:17 +0400

    test: Added first Qt 🧪
    build(tests-workflow): Removed Qt code from being included in coverage

---
+++ tests/qt/qt_example_test.cpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2025
+** qt_example_test.cpp
+** File description:
+** qt_example_test.cpp
+*/
+
+#include <qt6/QtTest/QtTest>
+
+class MyFirstTest: public QObject
+{
+    Q_OBJECT
+
+private:
+    bool myCondition()
+    {
+        return true;
+    }
+
+private slots:
+    void initTestCase()
+    {
+        qDebug("Called before everything else.");
+    }
+
+    void myFirstTest()
+    {
+        QVERIFY(true); // check that a condition is satisfied
+        QCOMPARE(1, 1); // compare two values
+    }
+
+    void mySecondTest()
+    {
+        QVERIFY(myCondition());
+        QVERIFY(1 != 2);
+    }
+
+    void cleanupTestCase()
+    {
+        qDebug("Called after myFirstTest and mySecondTest.");
+    }
+};
+
+QTEST_GUILESS_MAIN(MyFirstTest)
+#include "qt_example_test.moc"
