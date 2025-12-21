03149970c64e281885d2c087fc6ca17ec5e82866
NicolasReboule <nicolas.reboule@epitech.eu>
2025-12-21 18:20:49 +0400

    build(tests-workflow): Fixed Build and Run jobs on Linux
    chore: Linted qt_example_test.cpp

--- tests/qt/qt_example_test.cpp
+++ tests/qt/qt_example_test.cpp
@@ -7,36 +7,26 @@
 
 #include <qt6/QtTest/QtTest>
 
-class MyFirstTest: public QObject
-{
+class MyFirstTest : public QObject {
     Q_OBJECT
 
-private:
-    bool myCondition()
-    {
-        return true;
+   private:
+    bool myCondition() { return true; }
+
+   private slots:
+    void initTestCase() { qDebug("Called before everything else."); }
+
+    void myFirstTest() {
+        QVERIFY(true);   // check that a condition is satisfied
+        QCOMPARE(1, 1);  // compare two values
     }
 
-private slots:
-    void initTestCase()
-    {
-        qDebug("Called before everything else.");
-    }
-
-    void myFirstTest()
-    {
-        QVERIFY(true); // check that a condition is satisfied
-        QCOMPARE(1, 1); // compare two values
-    }
-
-    void mySecondTest()
-    {
+    void mySecondTest() {
         QVERIFY(myCondition());
         QVERIFY(1 != 2);
     }
 
-    void cleanupTestCase()
-    {
+    void cleanupTestCase() {
         qDebug("Called after myFirstTest and mySecondTest.");
     }
 };
