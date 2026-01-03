1c634f27ec87e864c605a52388963004201db4a7
NicolasReboule <NicolasReboule@users.noreply.github.com>
2026-01-03 09:37:17 +0000

    deploy: bb148faf6e2376d60324471fbfd45f0d33796e6d

--- search/all_b.js
+++ search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['nichanged_0',['NiChanged',['../classMTLMaterial.html#a14a680c0c23d8d1c560097471da1cef9',1,'MTLMaterial']]],
-  ['nschanged_1',['NsChanged',['../classMTLMaterial.html#a7353f24d8f8cdcb4491af9fdbe004d48',1,'MTLMaterial']]]
+  ['normalizepath_1',['normalizePath',['../StringHelpers_8hpp.html#a91e1e30e9a14940fe132f0b2c7bd72ec',1,'StringHelpers.hpp']]],
+  ['nschanged_2',['NsChanged',['../classMTLMaterial.html#a7353f24d8f8cdcb4491af9fdbe004d48',1,'MTLMaterial']]]
 ];
